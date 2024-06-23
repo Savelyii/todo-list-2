f4f91889a40b39afd3ba064c26a56c7e07c085ab
savelyi <leshaboichenko95@mail.ru>
2024-06-23 18:30:26 +0300

    add ToDo functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
+import { useState } from "react";
+import ToDoForm from "./ToDoForm";
+import ToDo from "./ToDo";
 
 function App() {
+  const [todos, setToDos] = useState([]);
+
+  const addTask = (userInput) => {
+    if (userInput) {
+      const newItem = {
+        id: Math.random().toString(36).substring(2, 9),
+        task: userInput,
+        complete: false,
+      };
+      setToDos([...todos, newItem]);
+    }
+  };
+
+  const removeTask = (id) => {
+    setToDos([...todos.filter((todo) => todo.id !== id)]);
+  };
+
+  const handleToggle = (id) => {
+    setToDos([
+      ...todos.map((todo) =>
+        todo.id === id ? { ...todo, complete: !todo.complete } : { ...todo }
+      ),
+    ]);
+  };
+
   return (
-<div className="App">
-  
-</div>
+    <div className="App">
+      <header>
+        <h1>Список задач: {todos.length}</h1>
+      </header>
+      <ToDoForm addTask={addTask} />
+      {todos.map((todo) => {
+        return (
+          <ToDo
+            todo={todo}
+            key={todo.id}
+            toggleTask={handleToggle}
+            removeTask={removeTask}
+          />
+        );
+      })}
+    </div>
   );
 }
 
